fb9bccc342da2290ad3222c588220efa608c9266
Hong Park <hongp26@gmail.com>
2017-07-10 16:37:51 -0400

    finish model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  belongs_to :book
 
   validates :sold?, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170710191235) do
+ActiveRecord::Schema.define(version: 20170710202156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
@@ -27,14 +25,24 @@ ActiveRecord::Schema.define(version: 20170710191235) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_books_on_user_id", using: :btree
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_genres_on_book_id", using: :btree
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "sold?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "start_time"
+    t.date     "end_time"
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,9 +64,7 @@ ActiveRecord::Schema.define(version: 20170710191235) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-
   add_foreign_key "books", "users"
-
+  add_foreign_key "genres", "books"
   add_foreign_key "orders", "users"
-
 end
