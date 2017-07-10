9025ee0ea7f5b82631e78ef988022b7c39d201d8
Raphael Regnier <raphael.regnier@hotmail.fr>
2017-07-10 17:16:21 -0400

    created controllers books and orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+
+  get 'orders/edit'
+
+  get 'books/new'
+
+  get 'books/show'
+
+  get 'books/index'
+
+  get 'books/edit'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
